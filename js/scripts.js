93922e79c9c72e708f2e6e39e3f0457c67af0334
postolenko <denis.postolenko@gmail.com>
2015-11-02 13:04:45 +0200

    Blik5

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 
 	
 	$("body").css({"padding-top": $(".headersite").height() + "px"});
-
 	
 
 	$(document).scroll(function() {
@@ -18,83 +17,60 @@ $(document).ready(function() {
 	for(var countServices = 0; countServices <= $(".service-photo").length - 1; ++countServices) {
 
 		$(".service-name:eq("+ countServices +")").css({"top": ( ($(".service-photo:eq("+ countServices +")").height() - $(".service-name:eq("+ countServices +")").outerHeight() ) / 2 ) + "px"});
-		// console..log($(".service-name:eq("+ countServices +")").css({"top"}) );
-		console.log(countServices);
-		console.log($(".service-photo:eq("+ countServices +")").height() +"   "+$(".service-name:eq("+ countServices +")").height());
-	}	console.log($(".service-photo:eq("+ countServices +")").height() - $(".service-name:eq("+ countServices +")").height());
 
-	console.log($(".service-name:eq(1)").index());
-
-
-	var rowWidth = $(".row").width();
-	var aboutarticleWidth = $(".service-type-box").outerWidth();
-	// var articlesLenght = $(".service-type-box").length - 1;
-
-	var countArticles;
-	var countRowsArticles = $(".service-type-box").length / countArticles;
-
-	var counterFor = 0;
-	var countRows;
+	}
 
 
 
 
+	// var rowWidth = $(".row").width();
+	// var aboutarticleWidth = $(".service-type-box").outerWidth();
+	// // var articlesLenght = $(".service-type-box").length - 1;
+
+	// var countArticles;
+	// var countRowsArticles = $(".service-type-box").length / countArticles;
+
+	// var counterFor = 0;
+	// var countRows;
 
 
 
 
+	// // delPaddings()
 
-	// delRightBorder();
+	// function delPaddings() {
 
-	function delRightBorder() {
+	// 	rowWidth = $(".row").width();
+	// 	aboutarticleWidth = $(".service-type-box").outerWidth();
+	// 	countArticles = Math.round(rowWidth / aboutarticleWidth);
+	// 	countRows = $(".service-type-box").length / countArticles;
 
-		rowWidth = $(".row").width();
-		aboutarticleWidth = $(".service-type-box").outerWidth();
-		countArticles = Math.round(rowWidth / aboutarticleWidth);
-		countRows = $(".service-type-box").length / countArticles;
+	// 	for(counterFor = 1; counterFor <= countRows; counterFor++) {
 
-		for(counterFor = 1; counterFor <= countRows; counterFor++) {
+	// 		$(".service-type-box:eq("+ ( counterFor * countArticles - 1 ) +")").css({
+	// 			"padding-right": 0
+	// 		});
 
-			$(".service-type-box:eq("+ ( counterFor * countArticles - 1 ) +")").css({
-				"padding-right": 0
-			});
+	// 		$(".service-type-box:eq("+ ( ( counterFor * countArticles - 1 ) - (countArticles - 1) ) +")").css({
+	// 			"padding-left": 0
+	// 		});
 
-			$(".service-type-box:eq("+ ( ( counterFor * countArticles - 1 ) - (countArticles - 1) ) +")").css({
-				"padding-left": 0
-			});
-
-		}
+	// 	}
 
 
-		if((countRows ^ 0) !== countRows) {
-			$(".service-type-box:eq("+ ( $(".service-type-box").length - 1 ) +")").css({
-				"padding-right": 0,
-				"padding-left": 0,
-			});
-		}
+	// 	if((countRows ^ 0) !== countRows) {
+	// 		$(".service-type-box:eq("+ ( $(".service-type-box").length - 1 ) +")").css({
+	// 			"padding-right": 0,
+	// 			"padding-left": 0,
+	// 		});
+	// 	}
 
-	}	
+	// }	
 
 
 	function getResponsiveHeader() {
 
-		// if($(window).scrollTop() >= $(".headersite").height() &&
-		// 	$(".top-head").hasClass("top-head-for-scrolling") != true && 
-		// 	$(".nav-link").removeClass("nav-link-for-scrolling") != true) {
-
-		// 	$(".top-head").addClass("top-head-for-scrolling");
-
-		// 	$(".nav-link").addClass("nav-link-for-scrolling");
-
-		// } else if( $(".top-head").hasClass("top-head-for-scrolling" == true) &&  
-		// 	       $(".nav-link").removeClass("nav-link-for-scrolling" == true)) {
-
-		// 	$(".top-head").removeClass("top-head-for-scrolling");
-
-		// 	$(".nav-link").removeClass("nav-link-for-scrolling");
-
-		// }
-	$("body").css({"padding-top": $(".headersite").height() + "px"});
+		$("body").css({"padding-top": $(".headersite").height() + "px"});
 
 		if($(window).scrollTop() >= $(".headersite").height() ) {
 
